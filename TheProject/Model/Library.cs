e467409e6e2b97fc843e9332c551f3a73143c746
Christian Schnöink <christian.schnoeink@rwe.com>
2018-09-06 10:40:21 +0200

    add Member to model

--- TheProject/Model/Library.cs
+++ TheProject/Model/Library.cs
@@ -1,8 +1,11 @@
-﻿namespace TheProject.Model
+﻿using System.Collections.Generic;
+
+namespace TheProject.Model
 {
     public class Library
     {
         public Stock Stock { get; set; } = new Stock();
 
+        public List<Member> Members { get; set; } = new List<Member>();
     }
 }
