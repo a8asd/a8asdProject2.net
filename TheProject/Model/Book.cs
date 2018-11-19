2734045a079cccaabb5011afd374439315b30370
david putman <davidputman@davidputman.com>
2018-11-19 23:00:52 +0000

    rebase

--- TheProject/Model/Book.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TheProject.Model
-{
-    public class Book
-    {
-        public string BookId { get; set;  }
-        public string Title { get; set; }
-        public string ISBN { get; set;  }
-    }
-}
