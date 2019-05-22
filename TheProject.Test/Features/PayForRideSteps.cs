754f49a06eabfcc66e41733298e012944507ffd6
UI432727 <UI432727@C028A2163>
2019-05-22 16:56:46 +0200

    Added PayForRide tests

---
+++ TheProject.Test/Features/PayForRideSteps.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace TheProject.Test.Features
+{
+    [Binding]
+    public class PayForRideSteps
+    {
+        [Given(@"Pat has travelled (.*) miles with Charlie")]
+        public void GivenPatHasTravelledMilesWithCharlie(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"the rate is £(.*) per mile")]
+        public void GivenTheRateIsPerMile(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"Pat pays for the ride")]
+        public void WhenPatPaysForTheRide()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"these are the invoices in the system")]
+        public void ThenTheseAreTheInvoicesInTheSystem(Table table)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
