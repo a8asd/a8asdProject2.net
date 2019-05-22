b45aeabc48f56c978179c475f8c26dc63ab60019
UI432727 <UI432727@C028A2163>
2019-05-22 11:09:49 +0200

    Sort out your sheet

--- TheProject.Test/Features/Booking.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TheProject.Test.Features
-{
-    internal class Booking
-    {
-        public Customer Customer { get; internal set; }
-        public Customer Driver { get; internal set; }
-    }
-}
